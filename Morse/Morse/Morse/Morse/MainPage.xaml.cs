6c96d77cb13f8d117cf09fa63c5b0c88bea25b8a
Matt Petermann <MPetermann2@my.wctc.edu>
2019-09-18 21:10:04 -0500

    Morse code

---
+++ Morse/Morse/Morse/Morse/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Morse
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<string> _codes = new List<string> { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "" };
+
+        private readonly List<char> _letters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Dot(object sender, EventArgs e) => Input.Text += ".";
+
+        public void Dash(object sender, EventArgs e) => Input.Text += "-";
+
+        public void Space(object sender, EventArgs e)
+        {
+            var idx = _codes.IndexOf(Input.Text);
+            Output.Text += idx == -1 ? ' ' : _letters[idx];
+            Input.Text = "";
+        }
+    }
+}
