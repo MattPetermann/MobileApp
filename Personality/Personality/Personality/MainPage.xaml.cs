f01d627b747c44642591502aa8c4003eb6b01e0c
Matt Petermann <MPetermann2@my.wctc.edu>
2019-10-08 23:37:50 -0500

    Recreate personality project

---
+++ Personality/Personality/Personality/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Personality
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
