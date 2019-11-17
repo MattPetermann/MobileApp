6b8dd88b1917bf0c5d4f6324d8bf494dfd499bd4
Matt <matthewp214@gmail.com>
2019-11-17 17:03:16 -0600

    Add like/dislike buttons

---
+++ localdb/localdb/localdb/ItemDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+namespace localdb
+{
+    public class ItemDatabase
+    {
+        private static ItemDatabase _database;
+        public static ItemDatabase Database =>
+            _database ?? (_database = new ItemDatabase(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "TodoSQLite.db3")));
+
+        public ItemDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<TodoItem>().Wait();
+        }
+    }
+}
