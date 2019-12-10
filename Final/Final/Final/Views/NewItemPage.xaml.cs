b93d1d7460fc8e1fac1a09216d21b2582e370b53
Matt <matthewp214@gmail.com>
2019-12-09 23:43:36 -0600

    .

--- Final/Final/Final/Views/NewItemPage.xaml.cs
+++ Final/Final/Final/Views/NewItemPage.xaml.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading;
 using Xamarin.Forms;
 using Final.Models;
 using Newtonsoft.Json.Linq;
